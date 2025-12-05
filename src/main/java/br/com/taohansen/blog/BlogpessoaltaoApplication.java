2e1d4b9fc829781e2d7aea27a909b4d575eb157f
taohansens <taohansens@gmail.com>
2025-12-04 22:45:42 -0300

    init project

---
+++ src/main/java/br/com/taohansen/blog/BlogpessoaltaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.taohansen.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoaltaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogpessoaltaoApplication.class, args);
+    }
+
+}
